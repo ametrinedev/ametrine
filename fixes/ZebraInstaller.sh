4904b692b942a879b4347b2b7ef46f5487a7eb31
dudeamax99 <Dudeamax99@yahoo.com>
2020-06-04 13:29:39 -0400

    Install script cleanup

--- fixes/ZebraInstaller.sh
+++ fixes/ZebraInstaller.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+echo ""
 echo "Installing zebra"
 curl -sSL https://github.com/wstyres/Zebra/releases/download/v1.1.10/xyz.willy.zebra_1.1.10_iphoneos-arm.deb -o zebra.deb
 dpkg -i zebra.deb
